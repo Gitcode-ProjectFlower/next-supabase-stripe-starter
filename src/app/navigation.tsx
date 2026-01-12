4c921651925c8e640e24496837f307bf844fddf5
CTbuidajon <olehkhomynn@gmail.com>
2026-01-12 22:08:42 +0200

    bugfix: build error

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -6,8 +6,7 @@ import { Logo } from '@/components/logo';
 import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTrigger } from '@/components/ui/sheet';
 import { getSession } from '@/features/account/controllers/get-session';
-
-import { signOut } from './(auth)/auth-actions';
+import { signOut } from './[locale]/(auth)/auth-actions';
 
 export async function Navigation() {
   const session = await getSession();
